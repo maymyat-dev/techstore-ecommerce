daf0f39ca75bdd96d9a520d910e494ad3152448f
mmm-maymyatmon <maymyat.dev@gmail.com>
2025-12-04 18:44:37 +0700

    build product details route

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,9 +1,93 @@
-import React from 'react'
+import VariantPicker from "@/components/products/variant-picker";
+import formatCurrency from "@/lib/formatCurrency";
+import { db } from "@/server";
+import { productVariants } from "@/server/schema";
+import { eq } from "drizzle-orm";
+import React from "react";
 
-const ProductDetailsPage = () => {
-  return (
-    <div>ProductDetailsPage</div>
-  )
+type ProductDetailsPageProps = {
+  params: {
+    id: number;
+  };
+};
+
+export async function generateStaticParams() {
+  const data = await db.query.productVariants.findMany({
+    with: {
+      product: true,
+      variantImages: true,
+      variantTags: true,
+    },
+  });
+  if (data) {
+    const idArr = data.map((product) => ({
+      id: product.id.toString(),
+    }));
+    return idArr;
+  }
+  return {};
 }
+const ProductDetailsPage = async ({ params }: ProductDetailsPageProps) => {
+  const productWithVariants = await db.query.productVariants.findFirst({
+    where: eq(productVariants.id, params.id),
+    with: {
+      product: {
+        with: {
+          productVariants: {
+            with: {
+              variantImages: true,
+              variantTags: true,
+            },
+          },
+        },
+      },
+    },
+  });
+  return (
+    <>
+      {productWithVariants && (
+        <main>
+          <div></div>
+          <div className="space-y-4">
+            <h2 className="text-2xl md:text-3xl font-semibold text-neutral-900 truncate">
+              {productWithVariants.product.title}
+            </h2>
 
-export default ProductDetailsPage
+            <div
+              className="prose prose-neutral max-w-none text-neutral-700"
+              dangerouslySetInnerHTML={{
+                __html: productWithVariants.product.description,
+              }}
+            />
+
+            <p className="text-sm text-neutral-600">
+              {productWithVariants.productType}
+            </p>
+
+            <p className="text-2xl font-semibold text-neutral-800">
+              {formatCurrency(productWithVariants.product.price)}
+            </p>
+
+            <div className="space-y-2">
+              <p className="text-sm font-medium text-neutral-700">Colors</p>
+              <div className="flex flex-wrap gap-3">
+                {productWithVariants.product.productVariants.map((v) => (
+                  <VariantPicker
+                    key={v.id}
+                    {...v}
+                    title={productWithVariants.product.title}
+                    price={productWithVariants.product.price}
+                    image={v.variantImages[0]?.image_url}
+                    productId={v.productId}
+                  />
+                ))}
+              </div>
+            </div>
+          </div>
+        </main>
+      )}
+    </>
+  );
+};
+
+export default ProductDetailsPage;
---
+++ components/products/variant-picker.tsx
@@ -0,0 +1,61 @@
+"use client";
+import { cn } from "@/lib/utils";
+import { useRouter, useSearchParams } from "next/navigation";
+import React from "react";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { Button } from "../ui/button";
+
+type VariantPickerProps = {
+  id: number;
+  color: string;
+  productType: string;
+  title: string;
+  price: number;
+  productId: number;
+  image: string;
+};
+const variantPicker = ({
+  id,
+  color,
+  productType,
+  title,
+  price,
+  productId,
+  image,
+}: VariantPickerProps) => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const selectedVariantColor = searchParams.get("type") || productType;
+  return (
+    <>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <div
+            onClick={() =>
+              router.push(
+                `/products/${id}?productId=${productId}&type=${productType}&image=${image}&title=${title}&price=${price}`,
+                { scroll: false }
+              )
+            }
+            style={{ backgroundColor: color }}
+            className={cn(
+              "w-8 h-8 rounded-full cursor-pointer",
+              selectedVariantColor === productType
+                ? "opacity-100"
+                : "opacity-50"
+            )}
+          ></div>
+        </TooltipTrigger>
+        <TooltipContent>
+          <p>{productType}</p>
+        </TooltipContent>
+      </Tooltip>
+    </>
+  );
+};
+
+export default variantPicker;
